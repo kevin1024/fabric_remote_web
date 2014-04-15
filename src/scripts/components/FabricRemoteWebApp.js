9efa0e18c30c7e0db357300e5128551a21aa788a
Kevin McCarthy <me@kevinmccarthy.org>
2014-04-14 20:25:04 -1000

    move state into top level thingy

--- src/scripts/components/FabricRemoteWebApp.js
+++ src/scripts/components/FabricRemoteWebApp.js
@@ -14,14 +14,28 @@ require('../../styles/main.css');
 var imageURL = '../../images/yeoman.png';
 var Tasklist = require('./Tasklist');
 var ExecutionList = require('./ExecutionList');
+var FabricRemote = require('fabric-remote');
 
 var FabricRemoteWebApp = React.createClass({
+  loadTasks: function() {
+    var fr = new FabricRemote('127.0.0.1', 1234, 'secret');
+    fr.listTasks().then(function(tasks) {
+      this.setState({tasks: tasks});
+    }.bind(this));
+  },
+  getInitialState: function() {
+    return {tasks: []};
+  },
+  componentWillMount: function() {
+    this.loadTasks();
+  },
   /*jshint ignore:start */
   render: function() {
+    var tasks=this.state.tasks;
     return (
       <div className='main'>
       <h1>Tasks</h1>
-        <Tasklist />
+        <Tasklist tasks={tasks}/>
       <h1>Executions</h1>
         <ExecutionList />
       </div>
--- src/scripts/components/Tasklist.js
+++ src/scripts/components/Tasklist.js
@@ -5,28 +5,15 @@
 'use strict';
 
 var React = require('react/addons');
-var FabricRemote = require('fabric-remote');
 var Task = require('./Task');
 
 var Tasklist = React.createClass({
-  loadTasks: function() {
-    var fr = new FabricRemote('localhost', 1234, 'secret');
-    fr.listTasks().then(function(tasks) {
-      this.setState({tasks: tasks});
-    }.bind(this));
-  },
-  getInitialState: function() {
-    return {tasks: []};
-  },
-  componentWillMount: function() {
-    this.loadTasks();
-  },
   
   /*jshint ignore:start */
   render: function () {
     var taskNodes = [];
-    for (var taskName in this.state.tasks) {
-      taskNodes.push(<Task name={taskName} description={this.state.tasks[taskName].description} />);
+    for (var taskName in this.props.tasks) {
+      taskNodes.push(<Task name={taskName} description={this.props.tasks[taskName].description} />);
     }
     return (
       <div className="taskList">
